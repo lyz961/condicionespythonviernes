e0d4821472425e699001023cf653a6cf5a245422
lyz961 <61318998+lyz961@users.noreply.github.com>
2023-08-18 10:43:15 -0500

    nt3 se hacen cambios

---
+++ condicion2.py
@@ -0,0 +1,8 @@
+temperatura=int(input("digite la temperatura:"))
+gradosAlcohol= int(input("digite el grado de alcohol"))
+                   
+if temperatura>50 or gradosAlcohol >5:
+    print("Alarma")
+else: 
+    print("normal")    
+                    
---
+++ condiciones.py
@@ -0,0 +1,7 @@
+edadUsuario= int(input("digita tu edad:"))
+if edadUsuario>=18:
+    print("Mayor de edad")
+else:
+    print("Menor de edad")
+    print("oeee para la casa")
+
